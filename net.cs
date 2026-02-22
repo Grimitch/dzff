97f7b8052ee95e96da59bf59fe267a43e2b183f4
Grimitch <156242381+Grimitch@users.noreply.github.com>
2026-02-22 22:12:39 +0100

    net to dapper+

---
+++ net.cs
@@ -0,0 +1,8 @@
+dotnet new console -n ExcelToDbApp
+cd ExcelToDbApp
+-----
+dotnet add package Dapper
+dotnet add package Z.Dapper.Plus
+dotnet add package ExcelDataReader
+dotnet add package ExcelDataReader.DataSet
+dotnet add package System.Data.SqlClient
